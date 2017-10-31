f34aa23cb9665ffe714928899688bbb565d47aed
fvasc <felipe_vasconcelos@outlook.com>
2017-10-31 21:28:06 -0200

    feat(ios): fix travis error

--- src/rules/ios.test.ts
+++ src/rules/ios.test.ts
@@ -6,88 +6,88 @@ declare const global: any;
 
 describe('Node info', () => {
 
-  let warnMock: jest.Mock<any>;
+    let warnMock: jest.Mock<any>;
 
-  beforeEach(() => {
-    warnMock = jest.fn();
-    global.warn = jest.fn(warnMock);
-    global.message = jest.fn();
-    global.fail = jest.fn();
-    global.markdown = jest.fn();
-  });
-
-  afterEach(() => {
-    global.warn = undefined;
-    global.message = undefined;
-    global.fail = undefined;
-    global.markdown = undefined;
-  });
-
-describe('Cakefile or settings.yml.erb or Fastfile', () => {
-    
-    it('Should warn when Cakefile is modified', async () => {
-      global.danger = {
-        git: {
-          modified_files: ['Cakefile', 'any'],
-          created_files: ['any'],
-        },
-      };
-
-      await ios.modifiedGradleOrManifest();
-
-      expect(global.warn).toBeCalled();
+    beforeEach(() => {
+        warnMock = jest.fn();
+        global.warn = jest.fn(warnMock);
+        global.message = jest.fn();
+        global.fail = jest.fn();
+        global.markdown = jest.fn();
     });
 
-    it('Should warn when settings.yml.erb is modified', async () => {
-      global.danger = {
-        git: {
-          modified_files: ['settings.yml.erb', 'any'],
-          created_files: ['any'],
-        },
-      };
-
-      await ios.modifiedGradleOrManifest();
-
-      expect(global.warn).toBeCalled();
+    afterEach(() => {
+        global.warn = undefined;
+        global.message = undefined;
+        global.fail = undefined;
+        global.markdown = undefined;
     });
 
-    it('Should warn when Fastfile is modified', async () => {
-        global.danger = {
-          git: {
-            modified_files: ['Fastfile', 'any'],
-            created_files: ['any'],
-          },
-        };
-  
-        await ios.modifiedGradleOrManifest();
-  
-        expect(global.warn).toBeCalled();
-      });
+    describe('Cakefile or settings.yml.erb or Fastfile', () => {
 
-    it('Should warn when Cakefile and settings.yml.erb and Fastfile are modified', async () => {
-      global.danger = {
-        git: {
-          modified_files: ['Cakefile', 'settings.yml.erb', 'Fastfile', 'any'],
-          created_files: ['any'],
-        },
-      };
+        it('Should warn when Cakefile is modified', async () => {
+            global.danger = {
+                git: {
+                    modified_files: ['Cakefile', 'any'],
+                    created_files: ['any'],
+                },
+            };
 
-      await ios.modifiedGradleOrManifest();
+            await ios.modifiedGradleOrManifest();
 
-      expect(global.warn).toHaveBeenCalledTimes(3);
+            expect(global.warn).toBeCalled();
+        });
+
+        it('Should warn when settings.yml.erb is modified', async () => {
+            global.danger = {
+                git: {
+                    modified_files: ['settings.yml.erb', 'any'],
+                    created_files: ['any'],
+                },
+            };
+
+            await ios.modifiedGradleOrManifest();
+
+            expect(global.warn).toBeCalled();
+        });
+
+        it('Should warn when Fastfile is modified', async () => {
+            global.danger = {
+                git: {
+                    modified_files: ['Fastfile', 'any'],
+                    created_files: ['any'],
+                },
+            };
+
+            await ios.modifiedGradleOrManifest();
+
+            expect(global.warn).toBeCalled();
+        });
+
+        it('Should warn when Cakefile and settings.yml.erb and Fastfile are modified', async () => {
+            global.danger = {
+                git: {
+                    modified_files: ['Cakefile', 'settings.yml.erb', 'Fastfile', 'any'],
+                    created_files: ['any'],
+                },
+            };
+
+            await ios.modifiedGradleOrManifest();
+
+            expect(global.warn).toHaveBeenCalledTimes(3);
+        });
+
+        it('Should not warn when Cakefile or settings.yml.erb or Fastfile are not modified', async () => {
+            global.danger = {
+                git: {
+                    modified_files: ['any'],
+                    created_files: ['any'],
+                },
+            };
+
+            await ios.modifiedGradleOrManifest();
+
+            expect(global.warn).not.toBeCalled();
+        });
     });
-
-    it('Should not warn when Cakefile or settings.yml.erb or Fastfile are not modified', async () => {
-      global.danger = {
-        git: {
-          modified_files: ['any'],
-          created_files: ['any'],
-        },
-      };
-
-      await ios.modifiedGradleOrManifest();
-
-      expect(global.warn).not.toBeCalled();
-    });
-  });
 });
--- src/rules/ios.ts
+++ src/rules/ios.ts
@@ -1,4 +1,4 @@
-import {DangerDSLType} from '../../node_modules/danger/distribution/dsl/DangerDSL';
+import { DangerDSLType } from '../../node_modules/danger/distribution/dsl/DangerDSL';
 declare const danger: DangerDSLType;
 
 export declare function message(message: string): void;
@@ -10,25 +10,24 @@ export declare function markdown(message: string): void;
  * ios rules
  */
 export let ios = {
-    
+
     /** Warn if some files/folders to be changed/committed like `Cakefile`, `settings.yml.erb`, `Fastfile` */
     async modifiedGradleOrManifest() {
         const hasFileWithPattern = (array: string[], pattern: RegExp): boolean => {
-          const fileChanged = array.findIndex(file => pattern.test(file)) > -1;
-          return fileChanged;
+            const fileChanged = array.findIndex(file => pattern.test(file)) > -1;
+            return fileChanged;
         };
-    
+
         if (hasFileWithPattern(danger.git.modified_files, /Cakefile/)) {
-          warn('This PR changes the "Cakefile" file.');
+            warn('This PR changes the "Cakefile" file.');
         }
-    
+
         if (hasFileWithPattern(danger.git.modified_files, /settings.yml.erb/)) {
-          warn('This PR changes the "settings.yml.erb" file.');
+            warn('This PR changes the "settings.yml.erb" file.');
         }
 
         if (hasFileWithPattern(danger.git.modified_files, /Fastfile/)) {
             warn('This PR changes the "Fastfile" file.');
-          }
-      },
-
-    };
+        }
+    },
+};
